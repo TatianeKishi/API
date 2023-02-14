d1eea0d9c5ac3a614c322d6c42be92521b6fedad
TatianeKishi <102524752+TatianeKishi@users.noreply.github.com>
2023-02-14 19:32:23 -0300

    Add files via upload

---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import jogos from "./jogosRoutes.js"
+import plataformas from "./plataformasRoutes.js"
+
+
+const routes = (app) => {
+    app.route('/').get((req, res) => {
+        res.status(200).send({titulo: "Aprendendo node"});
+    })
+    app.use(
+        express.json(),
+        jogos,
+        plataformas 
+                     
+    )
+}
+
+export default routes
+
+//aplicando as rotas
